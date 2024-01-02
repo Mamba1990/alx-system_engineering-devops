b21a72b46f5763dcf0c308db817cb740ff4e1fcb
Mamba1990 <h.daoudim@outlook.com>
2024-01-02 05:32:52 -0800

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module returning information for a given employee ID.
+
+About his/her TODO list progress from a REST API.
+"""
+
+import requests
+from sys import argv
+
+
+def fetch_data(id):
+    req = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                       .format(id))
+    user = req.json()
+    info = "Employee {} is done with tasks".format(user["name"])
+
+    req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                       .format(id))
+    todos = req.json()
+    info += "({}/{}):".format(
+                        sum(1 for todo in todos if todo["completed"]),
+                        len(todos))
+    for todo in todos:
+        if todo["completed"]:
+            info += "\n\t {}".format(todo["title"])
+    print(info)
+
+
+if __name__ == "__main__":
+    fetch_data(argv[1])

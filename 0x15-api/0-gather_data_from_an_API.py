3eb47619142ad9285ae222910a7146933f56c39e
Mamba1990 <h.daoudim@outlook.com>
2024-01-02 05:38:42 -0800

    gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,18 +13,18 @@ def fetch_data(id):
     req = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                        .format(id))
     user = req.json()
-    info = "Employee {} is done with tasks".format(user["name"])
+    inf = "Employee {} is done with tasks".format(user["name"])
 
     req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
                        .format(id))
     todos = req.json()
-    info += "({}/{}):".format(
+    inf += "({}/{}):".format(
                         sum(1 for todo in todos if todo["completed"]),
                         len(todos))
     for todo in todos:
         if todo["completed"]:
-            info += "\n\t {}".format(todo["title"])
-    print(info)
+            inf += "\n\t {}".format(todo["title"])
+    print(inf)
 
 
 if __name__ == "__main__":
